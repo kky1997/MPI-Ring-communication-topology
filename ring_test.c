7252bf727731ceed737ca60e05361a5144aa65a0
kky1997 <kaiyankoo@gmail.com>
2023-03-30 04:59:50 +1030

    changes to ring_test

--- ring_test.c
+++ ring_test.c
@@ -1,38 +1,57 @@
+#include <stdio.h> //standard io library
+#include <string.h> //string library
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char** argv) 
+int main(void) 
 {
-  // Initialize the MPI environment
-  MPI_Init(NULL, NULL);
-  // Find out rank, size
-  int world_rank, world_size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    
+    int m; //declare "m" variable
+    int comm_sz; //Number of processes
+    int my_rank; //My process rank'
 
-  // Initialize variable m to the process rank
-  int m = world_rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  // Perform left circular shift of m-values three times around the ring
-  for (int i = 0; i < 3; i++) {
-    int dest = (world_rank + 1) % world_size;
-    int source = (world_rank - 1 + world_size) % world_size;
-    int recv_m;
-    MPI_Send(&m, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-    MPI_Recv(&recv_m, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    m = recv_m;
-  }
+    MPI_Barrier(MPI_COMM_WORLD); // wait for all processes to synchronize before proceeding
+    
+    m = my_rank;
 
-/*
-  // Output m-values in order of rank
-  for (int i = 0; i < world_size; i++) {
-    if (world_rank == i) {
-      printf("Process %d has m-value %d\n", world_rank, m);
+    for(int q = 0; q < 3; q++) //loop 3 times to shift numbers 3 times
+    {
+      int recieved;
+      int destination;
+      int source;
+
+      if(my_rank != 0)
+      {
+        destination = my_rank - 1;
+        source = (my_rank - 1 + comm_sz) % comm_sz;
+        MPI_Recv(&recieved, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        m = recieved;
+      }
+      destination = (my_rank + 1) % comm_sz;
+      MPI_Send(&m, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
+
+      if(my_rank == 0)
+      {
+        source = comm_sz - 1;
+        MPI_Recv(&recieved, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+
+      MPI_Barrier(MPI_COMM_WORLD); // wait for all processes to synchronize before proceeding
     }
-    MPI_Barrier(MPI_COMM_WORLD); // synchronize processes
-  }
-*/
-  MPI_Finalize();
-  return 0;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    for(int i = 0; i < comm_sz; i++)
+    {
+        if(my_rank == i)
+        {
+            printf("Process %d holds the m-value %d\n", my_rank, m);
+        }
+        MPI_Barrier(MPI_COMM_WORLD); // wait for all processes to synchronize before proceeding
+    }
+       
+    MPI_Finalize();
+    return 0;
 }
